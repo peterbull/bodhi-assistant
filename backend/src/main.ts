484dbed617f1f25046c5ca5b16a3324e3ea3de13
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-06-19 13:06:36 -0400

    update swaggerui meta

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('Bodhi Assistant API')
+    .setDescription('AI assistant for the Bodhicast API')
+    .setVersion('0.1')
+    .addTag('bodhicast')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
